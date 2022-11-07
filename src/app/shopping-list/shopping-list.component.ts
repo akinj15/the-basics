d89a8d1bffafa0b42c4775f41cb1f82dc8313124
akin <akin.fantuci@innovaro.com.br>
2022-11-07 17:42:10 -0300

    terminando projeto basico para sempre

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredientes: Ingredient[] = [ 
+    new Ingredient('torta', 2),
+    new Ingredient('mopa', 2),
+    new Ingredient('virga', 2)
+  ]
   constructor() { }
 
   ngOnInit(): void {
