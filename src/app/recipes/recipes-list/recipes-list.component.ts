5a01d7719b3040fe45456e0afa07e46122a7f3b0
akin <akin.fantuci@innovaro.com.br>
2022-11-07 09:08:53 -0300

    atualizando tudo meu bem XD

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('figura repetida', 'quem faz oque faaz e triste, haha', 'https://static.wikia.nocookie.net/dragonball/images/4/4a/VegetaAscendedSuperSaiyanEp155.png/')
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
